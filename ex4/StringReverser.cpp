5a26b1ecf1408d17d30240a7037ce9c765303745
Ron123456 <wwwronwww1@gmail.com>
2020-01-20 19:41:18 -0500

    Removed directory of ex4 to new repository

--- ex4/StringReverser.cpp
+++
@@ -1,9 +0,0 @@
-#include "StringReverser.h"
-std::string StringReverser::solve(std::string p) {
-	int n = p.length();
-	std::string str(n, ' ');
-	for (int i = 0; i < n ; i++) {
-		str[i] = p[n - 1 - i];
-	}
-	return str;
-}
--- ex4/StringReverser.h
+++
@@ -1,5 +0,0 @@
-#include "interfaces.h"
-#include <string>
-class StringReverser : public  problem_solving::Solver<std::string, std::string>{
-	std::string solve(std::string s) override;
-};
